897cb76af4422ccb8fa92b28ebe5a1c0960f7f3d
venukotamraju <kotamraju.venugopal@gmail.com>
2023-10-19 16:08:56 +0530

    structured nav-bar and authLinks

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import styles from "./authLinks.module.css"
+import Link from "next/link";
 
 const AuthLinks = () => {
+    // temporary
+    const status = "notauthenticated"
     return (
-        <div className={styles.container}>AuthLinks</div>
+    <>
+        {status === "notauthenticated" ? (
+            <Link href = "/Login">Login</Link>
+        ):(
+            <>
+                <Link href="/write">Write</Link>
+                <span className={styles.link}>Logout</span>
+            </>
+        )}
+    </>
     )
 }
 
